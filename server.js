e41668491dcbf712360134c4491b3a4c84cc4a3d
Zhuk1305 <vaneczhuk@gmail.com>
2022-09-08 21:06:52 +0300

    fix: fix modal form for mobile

--- server.js
+++ server.js
@@ -5,29 +5,6 @@ const nodemailer = require("nodemailer");
 
 const PORT = process.env.PORT || 5000;
 
-// var crypto = require("crypto"),
-//   algorithm = "aes-256-ctr",
-//   password = "d6F3Efeq";
-
-// function encrypt(text) {
-//   var cipher = crypto.createCipher(algorithm, password);
-//   var crypted = cipher.update(text, "utf8", "hex");
-//   crypted += cipher.final("hex");
-//   return crypted;
-// }
-
-// function decrypt(text) {
-//   var decipher = crypto.createDecipher(algorithm, password);
-//   var dec = decipher.update(text, "hex", "utf8");
-//   dec += decipher.final("utf8");
-//   return dec;
-// }
-
-// var hw = encrypt("hello world");
-// // outputs hello world
-// console.log();
-// console.log(encrypt(hw));
-
 //Middleware
 app.use(express.static("public"));
 app.use(express.json());
